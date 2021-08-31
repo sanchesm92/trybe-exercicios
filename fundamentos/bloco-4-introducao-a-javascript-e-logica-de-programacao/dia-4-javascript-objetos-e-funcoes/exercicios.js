cebf54d281f7feddd045ba0421da58f1dea32954
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-08-30 21:18:07 -0300

    parte2 exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -87,9 +87,85 @@
 // }
 // console.log(maiorValor(array));
 
-1 pegar 1 elemento do array e atribuir a uma variavel 
-verificar o número de letras 
-pegar o maior numero de letras e retornar 
+// 1 pegar 1 elemento do array e atribuir a uma variavel 
+// verificar o número de letras 
+// pegar o maior numero de letras e retornar 
 
-let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-let verificar = "";
+// let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+// function maiorPalavra (n){
+// let verificar = n[0]
+// for (i in n){
+//   if (verificar.length < n[i].length){
+// verificar = n[i];
+//   }
+// }
+// return verificar
+// }
+// console.log(maiorPalavra(array));
+
+// let array = [2, 3, 2, 5, 8, 2, 3];
+
+// function repete(n){
+
+//   let indiceReposta = null;
+//   let verificar = null;
+//   let contador = 0;
+//   let contagem = 0;
+//   for (i in n){
+//     verificar = n[i];
+//     for (i2 in n){
+//         if (verificar === n[i2]){
+//           contador += 1;
+//         }
+//       }  
+//       if (contador > contagem){
+//         contagem = contador;
+//         indiceReposta = n[i];
+//       }
+//       contador = 0;
+//   }
+//   return indiceReposta;
+// };
+// console.log(repete(array));
+
+// function string(world, ending){
+//   let verificar1 = world.split('');
+//   let verificar2 = ending.split('');
+//   let contagem = 0;
+//   for ( i = 0; i < world.length; i += 1){
+//   verificar1 = world.length -i; 
+//   for (i2 = 0; i2 < ending.length; i2 += 1){
+//     verificar2 = ending.length - i2;
+//     if (verificar2 === verificar1){
+//       contagem +=1
+//     }
+//   }
+//   if ( contagem == ending.length -1){
+//   return true
+//   } else {
+//     return false
+//   }
+//   contagem = 0;
+// }
+// }
+// console.log( string('joaofernando', 'nando'));
+
+function verificar (palavra, fim){
+  let inversoPalavra = palavra.split('');
+  let reversoPalavra = inversoPalavra.reverse();
+  let palavraFinal = reversoPalavra.join('');
+  let inversoFim = fim.split('');
+  let reversoFim = inversoFim.reverse();
+  let fimFinal = reversoFim.join('');
+  let resposta = false;
+  for (i = 0; i < fimFinal.length; i += 1){
+    if ( palavraFinal[i] === fimFinal[i]){
+      resposta = true;
+    } else {
+      resposta = false;
+    }
+  }
+  return resposta;
+};
+console.log(verificar('joaofernando', 'fernand'));
