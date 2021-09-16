5073f675bd13f4b92f7d59552344835d7099ba1e
Felipe Sanches <sanchesmeirelles@gmail.com>
2021-09-16 13:18:17 -0300

    exercicios dia 6-1

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-responsivo/dia-1-html-e-css-forms/script.js
@@ -0,0 +1,58 @@
+const states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+const siglas = ['ac', 'al', 'am', 'am', 'ba', 'ce', 'df', 'es', 'go', 'ma', 'mt', 'ms', 'mg', 'pa', 'pb', 'pr', 'pe', 'pi', 'rj', 'rn', 'rs', 'ro', 'rr', 'sc', 'sp', 'se', 'to']
+const getStates = document.querySelector('#states');
+
+
+function putStates () {
+    for (let i = 0; i < states.length; i += 1){
+      let criaOption = document.createElement('option');
+      criaOption.innerText = states[i];
+      criaOption.value = siglas[i];
+      getStates.appendChild(criaOption);
+    }
+}
+putStates();
+
+// Bloquear limite maximo da data 
+
+// max e min data tirado ideia do site https://stackoverflow.com/questions/32378590/set-date-input-fields-max-date-to-today
+const getData = document.querySelector('#date')
+let today = new Date(); // gera a data de hoje sem ser em
+let day = today.getDate(); //esta pegando o dia
+let month = today.getMonth() +1; // está somando 1 no mes porque o janeiro é o 0 
+let year = today.getFullYear(); // está pegando o ano
+
+if (day < 10) { // caso o dia for menor que 10, inserir um 0 antes tipo dia 03
+   day = '0' + day;
+}
+
+if (month < 10) { // caso o mes for menor que 10 inserir um 0 antes
+  month = '0' + month;
+} 
+    
+today = year + '-' + month + '-' + day; // colocando no formato para atualizar o atributo max;
+getData.max = today;
+
+
+const getSubmit = document.querySelector('#submit');
+
+getSubmit.addEventListener('click', function (event) {
+event.preventDefault();
+let nome = document.querySelector('[name = nome');
+let cpf = document.querySelector('[name = cpf');
+let email = document.querySelector('[name = email');
+let address = document.querySelector('[name = address');
+let city = document.querySelector('[name = city');
+let resume = document.querySelector('[name = resume');
+let cargo = document.querySelector('[name = cargo');
+
+let description = document.querySelector('[name = description');
+let date = document.querySelector('[name = date');
+verificarLength();
+console.log(nome.value);
+})
+function verificarLength () {
+if ((nome.value.length > 40) || (cpf.value.length > 11) || (email.value.length > 50) || (address.value.length > 200) || (city.value.length > 28) || (resume.value.length > 1000) || (cargo.value.length > 40) || (description.value.length > 500)){
+  alert('Erro!')
+} 
+}
